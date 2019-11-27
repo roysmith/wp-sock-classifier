a57dd4f1272320ab9616ecc559a21d259e45b315
Roy Smith <roy@panix.com>
2019-11-27 01:05:35 +0000

    Add --dir option, to allow processing an entire directory.

--- utils/get_users_from_archive.py
+++ utils/get_users_from_archive.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Find information about a suspected sock:
+"""Extract information about suspected socks from a SPI case archive, including:
 
 * Username
 * Master?
@@ -8,21 +8,20 @@
 * Date of initial report
 * Date account registration
 * Date of first edit
+
 """
 
 import argparse
 import datetime
-import re
-import urllib.parse
-import itertools
+import sys
 import json
-
 from pathlib import Path
-from collections import Counter
 
 import mwclient
 import mwparserfromhell
 
+class ArchiveError(ValueError):
+    pass
 
 SITE = 'en.wikipedia.org'
 
@@ -42,25 +41,65 @@ MONTHS = {
 }
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--archive',
-                        help='SPI archive file to read',
-                        type=open,
-                        required=True)
+    parser = argparse.ArgumentParser(epilog='''If neither --archive nor --dir
+    are given, reads from stdin.''')
+    input_group = parser.add_mutually_exclusive_group()
+    input_group.add_argument('--archive',
+                             help='SPI archive file to read',
+                             default=sys.stdin,
+                             type=open)
+    input_group.add_argument('--dir',
+                             help='''Directory where SPI archives files
+                             can be found.  Each file in that directory
+                             will be processed in turn.''',
+                             type=directory_path)
     args = parser.parse_args()
 
     site = mwclient.Site(SITE)
     namespaces = {v: k for k, v in site.namespaces.items()}
 
-    wikicode = mwparserfromhell.parse(args.archive.read())
-    for username, spi_date, master_flag in parse_suspects(wikicode):
-        suspect = {
-            'username': username,
-            'master': master_flag,
-            'spi_date': spi_date_to_iso(spi_date),
-            }
+    if args.dir:
+        for path in args.dir.iterdir():
+            process_archive(path.open())
+    else:
+        process_archive(args.archive)
+
+
+def directory_path(arg):
+    "Type filter for argparse.add_argument()."
+    path = Path(arg)
+    if not path.is_dir():
+        raise argparse.ArgumentTypeError('not a directory')
+    return path
+
+
+def process_archive(stream):
+    for suspect in get_suspects(stream):
         print(json.dumps(suspect))
 
+def get_suspects(stream):
+    """Get the suspected socks from a stream containing an SPI archive.
+
+    Returns a list of suspects.
+
+    Returns an empty list and prints a diagnostic message on any kind
+    of parsing error.
+
+    """
+    suspects = []
+    wikicode = mwparserfromhell.parse(stream.read())
+    try:
+        for username, spi_date, master_flag in parse_suspects(wikicode):
+            suspect = {
+                'username': username,
+                'master': master_flag,
+                'spi_date': spi_date_to_iso(spi_date),
+            }
+            suspects.append(suspect)
+    except ArchiveError as ex:
+        print("Skipping %s: %s" % (stream.name, ex), file=sys.stderr)
+        return []
+    return suspects
 
 
 def parse_suspects(wikicode):
@@ -75,9 +114,11 @@ def parse_suspects(wikicode):
     """
 
     templates = wikicode.filter_templates(
-        matches = lambda template: template.name.matches('SPIarchive notice'))
-    if len(templates) != 1:
-        raise ValueError('expected exactly 1 SPIarchive notice')
+        matches=lambda template: template.name.matches('SPIarchive notice'))
+    count = len(templates)
+    if count != 1:
+        raise ArchiveError('expected exactly 1 SPIarchive notice, found %d' % count)
+
     master_username = templates[0].get(1).value.strip_code()
     master_pending = True
 
@@ -88,26 +129,26 @@ def parse_suspects(wikicode):
             master_pending = False
 
         templates = section.filter_templates(
-            matches = lambda template: template.name.matches('checkuser'))
+            matches=lambda template: template.name.matches('checkuser'))
         for template in templates:
             puppet_username = template.get(1).value.strip_code()
             yield (puppet_username, spi_date, False)
 
-        
+
 def spi_date_to_iso(spi_date):
 
     """Convert a string like "02 January 2018" into
     an ISO-8601 date string.
 
-    Raises ValueError if spi_date can't be converted.
+    Raises ArchiveError if spi_date can't be converted.
     """
     try:
         day, month_name, year = spi_date.split()
         month = MONTHS[month_name.lower()]
         return datetime.date(int(year), month, int(day)).isoformat()
     except:
-        raise ValueError("can't convert '%s'" % spi_date)
-    
-    
+        raise ArchiveError("can't convert date '%s'" % spi_date)
+
+
 if __name__ == '__main__':
     main()
