b7a12884934cbe087baebabdac8b4ebb23429983
Roy Smith <roy@panix.com>
2019-12-01 03:28:06 +0000

    Refactor to put all the per-archive logic in an Archive class.

--- utils/get_users_from_archive.py
+++ utils/get_users_from_archive.py
@@ -42,8 +42,6 @@ MONTHS = {
     'december': 12
 }
 
-logger = None
-
 def main():
     parser = argparse.ArgumentParser(epilog='''If neither --archive nor --archive_dir
     are given, reads from stdin.''')
@@ -65,17 +63,17 @@ def main():
 
     if args.archive_dir:
         for path in args.archive_dir.iterdir():
-            process_archive(path.open())
+            archive = Archive(path.open())
+            archive.process()
     else:
-        process_archive(args.archive)
+        archive = Archive(args.archive)
+        archive.process()
 
 
 def configure_logging(log_stream):
     logging.basicConfig(stream=log_stream, level=logging.INFO,
                         format='%(process)d %(asctime)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    global logger
-    logger = logging.getLogger()
 
 
 def directory_path(arg):
@@ -86,111 +84,117 @@ def directory_path(arg):
     return path
 
 
-def process_archive(stream):
-    for suspect in get_suspects(stream):
-        print(json.dumps(suspect))
+class Archive:
+    "An SPI case archive."
 
-def get_suspects(stream):
-    """Get the suspected socks from a stream containing an SPI archive.
+    def __init__(self, stream):
+        self.stream = stream
+        self.logger = logging.getLogger('archive')
+        self.site = mwclient.Site(SITE)
+    
+    def process(self):
+        for suspect in self.get_suspects():
+            print(json.dumps(suspect))
 
-    Returns a list of suspects.
+    def get_suspects(self):
+        """Get the suspected socks from a stream containing an SPI archive.
 
-    Returns an empty list and logs a diagnostic message on any kind
-    of parsing error.
+        Returns a list of suspects.
 
-    """
-    suspects = []
-    try:
-        for sock, spi_date, master in parse_suspects(stream):
-            suspect = {'master': master} if master else {}
-            suspect['sock'] = sock
-            suspect['spi_date'] = spi_date_to_iso(spi_date)
+        Returns an empty list and logs a diagnostic message on any kind
+        of parsing error.
 
-            reg_date = get_registration_date(sock)
-            if reg_date:
-                suspect['reg_time'] = reg_date.isoformat()
+        """
+        suspects = []
+        try:
+            for sock, spi_date, master in self.parse_suspects():
+                suspect = {'master': master} if master else {}
+                suspect['sock'] = sock
+                suspect['spi_date'] = self.spi_date_to_iso(spi_date)
 
-            suspects.append(suspect)
-        return suspects
-    except ArchiveError as ex:
-        logger.warning("Skipping %s: %s", stream.name, ex)
-        return []
-    except Exception as ex:
-        raise RuntimeError("error in %s" % stream.name) from ex
+                reg_date = self.get_registration_date(sock)
+                if reg_date:
+                    suspect['reg_time'] = reg_date.isoformat()
 
-
-def get_registration_date(username):
-    """Return a (UTC) datetime if a registration time can be found for the user.
-    If no entry can be found in the logs, return None.
-    """
-    site = mwclient.Site(SITE)
-    events = list(site.logevents(title='User:%s' % username, type='newusers'))
-    if len(events) == 0:
-        logger.info("Can't find newuser log entry for %s", username)
-        return
-    if len(events) > 1:
-        logger.warning("Multiple newuser log entries for %s", username)
-        return
-    timestamp = events[0]['timestamp']
-    return datetime.datetime.utcfromtimestamp(calendar.timegm(timestamp))
+                suspects.append(suspect)
+            return suspects
+        except ArchiveError as ex:
+            self.logger.warning("Skipping %s: %s", self.stream.name, ex)
+            return []
+        except Exception as ex:
+            raise RuntimeError("error in %s" % self.stream.name) from ex
 
 
-def parse_suspects(stream):
-    """Iterate over (sock, spi_date, master) tuples.
+    def get_registration_date(self, username):
+        """Return a (UTC) datetime if a registration time can be found for the user.
+        If no entry can be found in the logs, return None.
+        """
+        events = list(self.site.logevents(title='User:%s' % username, type='newusers'))
+        if len(events) == 0:
+            self.logger.info("Can't find newuser log entry for %s", username)
+            return
+        if len(events) > 1:
+            self.logger.warning("Multiple newuser log entries for %s", username)
+            return
+        timestamp = events[0]['timestamp']
+        return datetime.datetime.utcfromtimestamp(calendar.timegm(timestamp))
 
-    * sock is the account name of the suspected sock (no User:
-      prefix).
 
-    * spi_date is the date string in the format it appears in the SPI
-    case, i.e., '22 November 2019'.
+    def parse_suspects(self):
+        """Iterate over (sock, spi_date, master) tuples.
 
-    * master is the username of the suspected sockmaster.  For
-    sockmasters, this will be None, and sock wil hold the username.
+        * sock is the account name of the suspected sock (no User:
+          prefix).
 
-    """
-    wikicode = mwparserfromhell.parse(stream.read())
-    templates = wikicode.filter_templates(
-        matches=lambda template: template.name.matches('SPIarchive notice'))
-    count = len(templates)
-    if count != 1:
-        raise ArchiveError('expected exactly 1 SPIarchive notice, found %d' % count)
+        * spi_date is the date string in the format it appears in the SPI
+        case, i.e., '22 November 2019'.
 
-    master_username = templates[0].get(1).value.strip_code()
-    master_pending = True
+        * master is the username of the suspected sockmaster.  For
+        sockmasters, this will be None, and sock wil hold the username.
 
-    for section in wikicode.get_sections(levels=[3]):
-        spi_date = section.filter_headings()[0].title
-        if master_pending:
-            # We havn't printed the data from the {{SPIarchive
-            # notice}} yet, so print it now.  We needed to delay
-            # printing that until we processed the first level-3
-            # heading, because that's when we discover the date of the
-            # first report.
-            yield (master_username, spi_date, None)
-            master_pending = False
+        """
+        wikicode = mwparserfromhell.parse(self.stream.read())
+        templates = wikicode.filter_templates(
+            matches=lambda template: template.name.matches('SPIarchive notice'))
+        count = len(templates)
+        if count != 1:
+            raise ArchiveError('expected exactly 1 SPIarchive notice, found %d' % count)
 
-        templates = section.filter_templates(
-            matches=lambda template: template.name.matches('checkuser'))
-        for template in templates:
-            if template.has(1):
-                puppet_username = template.get(1).value.strip_code()
-                yield (puppet_username, spi_date, master_username)
-            else:
-                logger.warning("Skipping template (%s), missing param: %s", stream.name, template)
+        master_username = templates[0].get(1).value.strip_code()
+        master_pending = True
 
-def spi_date_to_iso(spi_date):
+        for section in wikicode.get_sections(levels=[3]):
+            spi_date = section.filter_headings()[0].title
+            if master_pending:
+                # We havn't printed the data from the {{SPIarchive
+                # notice}} yet, so print it now.  We needed to delay
+                # printing that until we processed the first level-3
+                # heading, because that's when we discover the date of the
+                # first report.
+                yield (master_username, spi_date, None)
+                master_pending = False
 
-    """Convert a string like "02 January 2018" into
-    an ISO-8601 date string.
+            templates = section.filter_templates(
+                matches=lambda template: template.name.matches('checkuser'))
+            for template in templates:
+                if template.has(1):
+                    puppet_username = template.get(1).value.strip_code()
+                    yield (puppet_username, spi_date, master_username)
+                else:
+                    self.logger.warning("Skipping template (%s), missing param: %s", stream.name, template)
 
-    Raises ArchiveError if spi_date can't be converted.
-    """
-    try:
-        day, month_name, year = spi_date.split()
-        month = MONTHS[month_name.lower()]
-        return datetime.date(int(year), month, int(day)).isoformat()
-    except:
-        raise ArchiveError("can't convert date '%s'" % spi_date)
+    def spi_date_to_iso(self, spi_date):
+        """Convert a string like "02 January 2018" into
+        an ISO-8601 date string.
+
+        Raises ArchiveError if spi_date can't be converted.
+        """
+        try:
+            day, month_name, year = spi_date.split()
+            month = MONTHS[month_name.lower()]
+            return datetime.date(int(year), month, int(day)).isoformat()
+        except:
+            raise ArchiveError("can't convert date '%s'" % spi_date)
 
 
 if __name__ == '__main__':
