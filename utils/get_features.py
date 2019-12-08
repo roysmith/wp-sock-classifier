b12ed739fe6ec07f3c467948ca6cafc4c4f72829
Roy Smith <roy@panix.com>
2019-12-08 03:11:04 +0000

    Eliminate the spi_date field.  It probably was never going to be used for anything, and eliminating it reduces a fair amount of code complexity.

--- utils/get_features.py
+++ utils/get_features.py
@@ -134,10 +134,9 @@ class Archive:
         """
         suspects = []
         try:
-            for sock, spi_date, master in self.parse_suspects():
+            for sock, master in self.parse_suspects():
                 suspect = {'master': master} if master else {}
                 suspect['sock'] = sock
-                suspect['spi_date'] = self.spi_date_to_iso(spi_date)
 
                 reg_date = self.get_registration_date(sock)
                 if reg_date:
@@ -280,13 +279,10 @@ class Archive:
 
 
     def parse_suspects(self):
-        """Iterate over (sock, spi_date, master) tuples.
+        """Iterate over (sock, master) tuples.
 
         * sock is the account name of the suspected sock (no User:
           prefix).
-
-        * spi_date is the date string in the format it appears in the SPI
-        case, i.e., '22 November 2019'.
 
         * master is the username of the suspected sockmaster.  For
         sockmasters, this will be None, and sock wil hold the username.
@@ -300,42 +296,17 @@ class Archive:
             raise ArchiveError('expected exactly 1 SPIarchive notice, found %d' % count)
 
         master_username = templates[0].get(1).value.strip_code()
-        master_pending = True
+        yield (master_username, None)
 
-        for section in wikicode.get_sections(levels=[3]):
-            spi_date = section.filter_headings()[0].title
-            self.logger.debug("Processing %s", spi_date)
-            if master_pending:
-                # We havn't emitted the data from the {{SPIarchive
-                # notice}} yet, so emit it now.  We needed to delay
-                # emitting that until we processed the first level-3
-                # heading, because that's when we discover the date of
-                # the first report.
-                yield (master_username, spi_date, None)
-                master_pending = False
-
-            templates = section.filter_templates(
-                matches=lambda template: template.name.matches('checkuser'))
-            for template in templates:
-                if template.has(1):
-                    puppet_username = template.get(1).value.strip_code()
-                    self.logger.debug("Found %s", puppet_username)
-                    yield (puppet_username, spi_date, master_username)
-                else:
-                    self.logger.warning("Skipping template (%s), missing param: %s", self.stream.name, template)
-
-    def spi_date_to_iso(self, spi_date):
-        """Convert a string like "02 January 2018" into
-        an ISO-8601 date string.
-
-        Raises ArchiveError if spi_date can't be converted.
-        """
-        try:
-            day, month_name, year = spi_date.split()
-            month = MONTHS[month_name.lower()]
-            return datetime.date(int(year), month, int(day)).isoformat()
-        except:
-            raise ArchiveError("can't convert date '%s'" % spi_date)
+        templates = wikicode.filter_templates(
+            matches=lambda template: template.name.matches('checkuser'))
+        for template in templates:
+            if template.has(1):
+                puppet_username = template.get(1).value.strip_code()
+                self.logger.debug("Found %s", puppet_username)
+                yield (puppet_username, master_username)
+            else:
+                self.logger.warning("Skipping template (%s), missing param: %s", self.stream.name, template)
 
 
 if __name__ == '__main__':
